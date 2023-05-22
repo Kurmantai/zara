eec14f894495e988a997c2ccfbffda2be371757c
Nur4iksadykov <nur4ik.sadykov@gmail.com>
2023-05-22 19:40:52 +0600

    check

--- src/components/pages/AuthPage.jsx
+++ src/components/pages/AuthPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+// import "./auth.css";
 
 const AuthPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>login page</h1>
+      <div className="login">
+        <input type="text" placeholder="E-MAIL" />
+        <input type="text" placeholder="PASSWORD" />
+        <button>LOG IN</button>
+      </div>
+    </div>
+  );
 };
 
 export default AuthPage;
