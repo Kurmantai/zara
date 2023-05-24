aa5951fcdf2033fda75a9125089519848db4072b
Akulov <arxkuma@gmail.com>
2023-05-24 19:44:16 +0600

    half way

--- src/components/pages/AddProductPage/AddProductPage.jsx
+++ src/components/pages/AddProductPage/AddProductPage.jsx
@@ -1,52 +1,133 @@
-import React from "react";
-import "./AddProductPage.scss";
-import { motion } from "framer-motion";
-import Navbar from "../../mainComponents/navbar/Navbar";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useProduct } from "../../contexts/ProductContextProvider";
 import { Link } from "react-router-dom";
+import "./AddProductPage.scss";
 
 const AddProductPage = () => {
+  const { addProduct } = useProduct();
+
+  const [formValue, setFormValue] = useState({
+    title: "",
+    description: "",
+    price: "",
+    category: "",
+    image: "",
+  });
+
+  function handleChange(e) {
+    let obj = {
+      ...formValue,
+      [e.target.name]: e.target.value,
+    };
+    setFormValue(obj);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (
+      !formValue.title.trim() ||
+      !formValue.description.trim() ||
+      !formValue.price.trim() ||
+      !formValue.image.trim() ||
+      !formValue.category.trim()
+    ) {
+      alert("fill the fields");
+      return;
+    }
+
+    addProduct(formValue);
+  }
+
   return (
-    <motion.div
-      className="AddPro"
-      initial={{
-        opacity: 0,
-        transform: "scale(0.5)",
-        filter: "blur(10px)",
-      }}
-      animate={{
-        opacity: 1,
-        transform: "scale(1)",
-        filter: "blur(0px)",
-        transition: { duration: 0.4, ease: "easeOut" },
-      }}
-      exit={{
-        opacity: 0,
-        transform: "scale(0.5)",
-        filter: "blur(10px)",
-        transition: { duration: 0.2, ease: "easeIn" },
-      }}
-    >
-      <img className="add__bg" src="./images/bg6.jpg" alt="bg" />
-      <div className="container__add_overlay"></div>
+    <div className="add__main_container">
+      <div style={{ height: "10%" }}></div>
+      <Link to="/">
+        <img src="/images/zara.svg" alt="image" />
+      </Link>
+      <h1
+        style={{
+          textAlign: "center",
+          marginTop: "1.5%",
+        }}
+      >
+        ADD PRODUCT
+      </h1>
 
-      <div className="AddBody">
-        <Link to="/">
-          <img src="./images/zara.svg" alt="img" />
-        </Link>
-        <Link to="/">
-          <button className="get__back">GET BACK</button>
-        </Link>
-
-        <h3>ADD PRODUCT</h3>
-        <input placeholder="Title"></input>
-
-        <input placeholder="Price"></input>
-        <input placeholder="Image"></input>
-        <input placeholder="Category"></input>
-        <button className="add__btn">aDD PRODUCT</button>
-        <div></div>
-      </div>
-    </motion.div>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        style={{
+          maxWidth: "500px",
+          margin: "0 auto",
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+        }}
+      >
+        <TextField
+          sx={{ marginTop: "5%" }}
+          value={formValue.title}
+          onChange={(e) => handleChange(e)}
+          name="title"
+          label="Title"
+          variant="outlined"
+        />
+        <TextField
+          value={formValue.description}
+          onChange={(e) => handleChange(e)}
+          name="description"
+          label="Description"
+          variant="outlined"
+        />
+        <TextField
+          value={formValue.price}
+          onChange={(e) => handleChange(e)}
+          name="price"
+          label="Price"
+          variant="outlined"
+        />
+        <TextField
+          value={formValue.image}
+          onChange={(e) => handleChange(e)}
+          name="image"
+          label="Image"
+          variant="outlined"
+        />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Category</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Category"
+            name="category"
+            value={formValue.category}
+            onChange={(e) => handleChange(e)}
+          >
+            <MenuItem value={"electronics"}>Electronics</MenuItem>
+            <MenuItem value={"jewelry"}>Jewelry</MenuItem>
+            <MenuItem value={"books"}>Books</MenuItem>
+          </Select>
+        </FormControl>
+        <Button
+          style={{
+            height: "50px",
+            width: "100%",
+            color: "white",
+            backgroundColor: "black",
+          }}
+          type="submit"
+        >
+          Send Product
+        </Button>
+      </form>
+    </div>
   );
 };
 
--- src/components/pages/EditProductPage/EditProductPage.jsx
+++ src/components/pages/EditProductPage/EditProductPage.jsx
@@ -1,14 +1,73 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useProduct } from "../../contexts/ProductContextProvider";
+import { useNavigate, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./EditProductPage.scss";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
 
 const EditProductPage = () => {
+  const { oneProduct, getOneProduct, editProduct } = useProduct();
+
+  const params = useParams();
+
+  const navigate = useNavigate();
+
+  const [formValue, setFormValue] = useState({
+    title: "",
+    description: "",
+    price: "",
+    category: "",
+    image: "",
+  });
+
+  useEffect(() => {
+    getOneProduct(params.id);
+  }, []);
+
+  useEffect(() => {
+    if (oneProduct) {
+      setFormValue(oneProduct);
+    }
+  }, [oneProduct]);
+
+  function handleChange(e) {
+    let obj = {
+      ...formValue,
+      [e.target.name]: e.target.value,
+    };
+    setFormValue(obj);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (
+      !formValue.title ||
+      !formValue.description ||
+      !formValue.price ||
+      !formValue.image ||
+      !formValue.category
+    ) {
+      alert("fill the fields");
+      return;
+    }
+
+    editProduct(formValue, params.id);
+    navigate("/");
+  }
+
   return (
     <motion.div
       initial={{
         opacity: 0,
-        transform: "scale(0.5)",
+        transform: "scale(0.3)",
         filter: "blur(10px)",
       }}
       animate={{
@@ -19,29 +78,84 @@ const EditProductPage = () => {
       }}
       exit={{
         opacity: 0,
-        transform: "scale(0.5)",
+        transform: "scale(0.3)",
         filter: "blur(10px)",
         transition: { duration: 0.2, ease: "easeIn" },
       }}
+      className="edit__main_container"
     >
-      <div className="edit__main_container">
-        <div className="edit__navbar">
-          <Link to="/">
-            <img className="edit__logo" src="/images/za.svg" alt="img" />
-          </Link>
-          <h1>EDIT YOUR PRODUCT</h1>
-        </div>
-        <div className="edit__body">
-          <div>
-            <input type="text" placeholder="TITLE" />
-            <input type="text" placeholder="PRICE" />
-            <input type="text" placeholder="IMAGE URL" />
-            <input type="text" placeholder="CATEGORY" />
-          </div>
+      <div style={{ height: "10%" }}></div>
+      <Link to="/">
+        <img src="/images/zara.svg" alt="image" />
+      </Link>
 
-          <button>EDIT PRODUCT</button>
-        </div>
-      </div>
+      <h1 style={{ textAlign: "center", marginTop: "1.5%" }}>Edit Product</h1>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        style={{
+          maxWidth: "500px",
+          margin: "0 auto",
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+        }}
+      >
+        <TextField
+          sx={{ marginTop: "5%" }}
+          value={formValue.title}
+          onChange={(e) => handleChange(e)}
+          name="title"
+          label="Title"
+          variant="outlined"
+        />
+        <TextField
+          value={formValue.descrption}
+          onChange={(e) => handleChange(e)}
+          name="description"
+          label="Description"
+          variant="outlined"
+        />
+        <TextField
+          value={formValue.price}
+          onChange={(e) => handleChange(e)}
+          name="price"
+          label="Price"
+          variant="outlined"
+        />
+        <TextField
+          value={formValue.image}
+          onChange={(e) => handleChange(e)}
+          name="image"
+          label="Image"
+          variant="outlined"
+        />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Category</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Category"
+            name="category"
+            value={formValue.category}
+            onChange={(e) => handleChange(e)}
+          >
+            <MenuItem value={"electronics"}>Electronics</MenuItem>
+            <MenuItem value={"jewelry"}>Jewelry</MenuItem>
+            <MenuItem value={"books"}>Books</MenuItem>
+          </Select>
+        </FormControl>
+        <Button
+          style={{
+            height: "50px",
+            width: "100%",
+            color: "white",
+            backgroundColor: "black",
+          }}
+          type="submit"
+        >
+          EDIT PRODUCT
+        </Button>
+      </form>
     </motion.div>
   );
 };
